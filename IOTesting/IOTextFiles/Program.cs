b338ccdde574e1799bd83af66f39f586e3a02c6b
Petar Georgiev <pkg_vt@abv.bg>
2015-11-21 16:48:35 +0200

    Модул 08, Видео 03, Четене на текстов файл

--- IOTesting/IOTextFiles/Program.cs
+++ IOTesting/IOTextFiles/Program.cs
@@ -10,14 +10,16 @@ namespace IOTextFiles
 			STable _STable = new STable ();
 			IOSettings _io = new IOSettings (_STable);
 
-			Console.WriteLine (_STable.stable[0]);
 
-			if (_io.save ()) {
-				Console.WriteLine ("Таблицата е запаметена успешно.");
+
+			if ( _io.open()){                 //_io.save ()) {
+				Console.WriteLine ("Таблицата е прочетена успешно.");
 
 			} else {
-				Console.WriteLine ("Таблицата НЕ е запаметена успешно.");
+			Console.WriteLine ("Таблицата НЕ е прочетена успешно.");
 			}
+			Console.WriteLine (_STable.stable[2]);
+
 		}
 	}
 }
